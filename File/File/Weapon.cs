b2dfe81246aeecbbbaa0aa3c1d82415926c6a42a
Baramlock <87575785+Baramlock@users.noreply.github.com>
2021-12-22 19:13:49 +0500

    Оружие

---
+++ File/File/Weapon.cs
@@ -0,0 +1,36 @@
+﻿internal class Weapon
+{
+    public int Damage { get; private set; }
+    public int Bullets { get; private set; }
+
+    public void Fire(Player player)
+    {
+        player.TakeDamage(Damage);
+        Bullets -= 1;
+    }
+}
+
+internal class Player
+{
+    public int Health { get; private set; }
+
+    public void TakeDamage(int damage)
+    {
+        Health -= damage;
+    }
+}
+
+internal class Bot
+{
+    private Weapon _Weapon;
+
+    public Bot(Weapon weapon)
+    {
+        _Weapon = weapon;
+    }
+
+    public void OnSeePlayer(Player player)
+    {
+        _Weapon.Fire(player);
+    }
+}
