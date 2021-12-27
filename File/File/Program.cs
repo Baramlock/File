045286f947ea89f40217c325df872d98a6c7b5b4
Baramlock <87575785+Baramlock@users.noreply.github.com>
2021-12-27 13:49:19 +0500

    7. Магические числа всегда нужно заменять на константы

--- File/File/Program.cs
+++ File/File/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Weapon
+{
+    private int _bullets;
+    private const int _bulletsToShoot = 1;
+
+    public bool CanShoot() => _bullets >= _bulletsToShoot;
+
+    public void Shoot() => _bullets -= _bulletsToShoot;
+}
