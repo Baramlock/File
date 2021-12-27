818e089ab2536716187b8846db7db794cea7bda1
Baramlock <87575785+Baramlock@users.noreply.github.com>
2021-12-27 14:13:28 +0500

    17. Методы должны устанавливать значения из параметра

--- File/File/Program.cs
+++ File/File/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public static void CreateObject()
+{
+    //Создание объекта на карте
+}
+
+public static void GetChance()
+{
+    _chance = Random.Range(0, 100);
+}
+
+public static int СalculateSalary(int hoursWorked)
+{
+    return _hourlyRate * hoursWorked;
+}
