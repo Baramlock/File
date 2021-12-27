227fa8bb3b9536fe9980061d45a2366789f8a5ea
Baramlock <87575785+Baramlock@users.noreply.github.com>
2021-12-27 11:02:07 +0500

    Логирование

--- File/File/Logging.cs
+++ File/File/Logging.cs
@@ -1,40 +1,96 @@
-﻿using System;
-using System.IO;
-
-namespace Lesson
+﻿namespace Lesson
 {
-    class Logging
+    internal class Logging
     {
         static void Main(string[] args)
         {
-
+            Pathfinder writeInFile = new Pathfinder(new FileLogWritter(new PolicySendfAlways()));
+            Pathfinder writeInConsole = new Pathfinder(new ConsoleLogWritter(new PolicySendfAlways()));
+            Pathfinder fridayWriteInFile = new Pathfinder(new FileLogWritter(new PolicySendIfFriday()));
+            Pathfinder fridayWriteInConsole = new Pathfinder(new ConsoleLogWritter(new PolicySendIfFriday()));
+            Pathfinder writeInConsoleInFridayToFile = new Pathfinder(new ConsoleLogWritter(new PolicySendIfFriday()), new FileLogWritter(new PolicySendfAlways()));
         }
     }
 
-    class ConsoleLogWritter
+    interface ILogger
     {
-        public virtual void WriteError(string message)
+        void WriteMessage(string message);
+    }
+
+    interface IWritePolicy
+    {
+        bool IsWrite();
+    }
+
+    internal class PolicySendfAlways : IWritePolicy
+    {
+        public bool IsWrite() => true;
+    }
+
+    internal class PolicySendIfFriday : IWritePolicy
+    {
+        public bool IsWrite() => DateTime.Now.DayOfWeek == DayOfWeek.Friday;
+    }
+
+    internal class Pathfinder : ILogger
+    {
+        private readonly IEnumerable<ILogger> _loggers;
+
+        public Pathfinder(params ILogger[] loggers)
+        {
+            _loggers = loggers;
+        }
+
+        public void WriteMessage(string message)
+        {
+            foreach (var logger in _loggers)
+            {
+                logger.WriteMessage(message);
+            }
+        }
+
+        public void Find(string message, ILogger logger)
+        {
+            logger.WriteMessage(message);
+        }
+    }
+
+    internal class ConsoleLogWritter : LogWritter
+    {
+        public ConsoleLogWritter(IWritePolicy writePolicy) : base(writePolicy) { }
+
+        protected override void Write(string message)
         {
             Console.WriteLine(message);
         }
     }
 
-    class FileLogWritter
+    internal class FileLogWritter : LogWritter
     {
-        public virtual void WriteError(string message)
+        public FileLogWritter(IWritePolicy writePolicy) : base(writePolicy) { }
+
+        protected override void Write(string message)
         {
             File.WriteAllText("log.txt", message);
         }
     }
 
-    class SecureConsoleLogWritter : ConsoleLogWritter
+    internal abstract class LogWritter : ILogger
     {
-        public override void WriteError(string message)
+        protected readonly IWritePolicy _writePolicy;
+
+        public LogWritter(IWritePolicy writePolicy)
         {
-            if (DateTime.Now.DayOfWeek == DayOfWeek.Friday)
-            {
-                base.WriteError(message);
-            }
+            _writePolicy = writePolicy;
         }
+
+        public void WriteMessage(string message)
+        {
+            if (_writePolicy.IsWrite())
+                Write(message);
+
+        }
+
+        protected abstract void Write(string message);
     }
 }
