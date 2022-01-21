7359e02ca038e38842da4c9150a93934a56dc5dd
Ямковой Данил <danil1998.01.08@outlook.com>
2022-01-21 14:50:24 +0500

    2 методав вкл выкл

--- File/File/Program.cs
+++ File/File/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public void SetOn()
+{
+    _effects.StartEnableAnimation();
+}
+
+public void SetOff()
+{
+    _pool.Free(this);
+}
