0a13475c4b8d61844060077c00e8d78b01645c90
Ямковой Данил <danil1998.01.08@outlook.com>
2022-01-21 15:05:21 +0500

    Имя параметра дублируется в имени метода старт

--- File/File/Program.cs
+++ File/File/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public void ShootPlayer(Player player) { }
+public string FindByIndex(int number) { }
