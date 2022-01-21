05c1bcfeaf8f5ee29512e300e8200406df1cd8bb
Ямковой Данил <danil1998.01.08@outlook.com>
2022-01-21 14:50:24 +0500

    Старт

--- File/File/Program.cs
+++ File/File/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+public void SetEnable(bool enable)
+{
+    _enable = enable;
+
+    if (_enable)
+    {
+        _effects.StartEnableAnimation();
+    }
+    else
+    {
+        _pool.Free(this);
+    }
+}
