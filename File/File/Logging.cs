053f99635cb194f94cf87f01292203f73a5fca71
Ямковой Данил <danil1998.01.08@outlook.com>
2022-01-27 08:06:27 +0500

    Слтяние

--- File/File/Logging.cs
+++ File/File/Logging.cs
@@ -8,7 +8,9 @@
             Pathfinder writeInConsole = new Pathfinder(new ConsoleLogWritter(new PolicySendfAlways()));
             Pathfinder fridayWriteInFile = new Pathfinder(new FileLogWritter(new PolicySendIfFriday()));
             Pathfinder fridayWriteInConsole = new Pathfinder(new ConsoleLogWritter(new PolicySendIfFriday()));
-            Pathfinder writeInConsoleInFridayToFile = new Pathfinder(new ConsoleLogWritter(new PolicySendIfFriday()), new FileLogWritter(new PolicySendfAlways()));
+            Pathfinder writeInConsoleInFridayToFile = new Pathfinder(new LogsWriter(
+                new ConsoleLogWritter(new PolicySendIfFriday()),
+                new FileLogWritter(new PolicySendfAlways())));
         }
     }
 
@@ -17,36 +19,34 @@
         void WriteMessage(string message);
     }
 
-    interface IWritePolicy
+    interface ICanWrite
     {
         bool IsWrite();
     }
 
-    internal class PolicySendfAlways : IWritePolicy
+    internal class PolicySendfAlways : ICanWrite
     {
         public bool IsWrite() => true;
     }
 
-    internal class PolicySendIfFriday : IWritePolicy
+    internal class PolicySendIfFriday : ICanWrite
     {
         public bool IsWrite() => DateTime.Now.DayOfWeek == DayOfWeek.Friday;
     }
 
     internal class Pathfinder : ILogger
     {
-        private readonly IEnumerable<ILogger> _loggers;
+        private readonly ILogger _loggers;
 
-        public Pathfinder(params ILogger[] loggers)
+        public Pathfinder(ILogger loggers)
         {
             _loggers = loggers;
         }
 
         public void WriteMessage(string message)
         {
-            foreach (var logger in _loggers)
-            {
-                logger.WriteMessage(message);
-            }
+
+            _loggers.WriteMessage(message);
         }
 
         public void Find(string message, ILogger logger)
@@ -57,7 +57,7 @@
 
     internal class ConsoleLogWritter : LogWritter
     {
-        public ConsoleLogWritter(IWritePolicy writePolicy) : base(writePolicy) { }
+        public ConsoleLogWritter(ICanWrite writePolicy) : base(writePolicy) { }
 
         protected override void Write(string message)
         {
@@ -67,7 +67,7 @@
 
     internal class FileLogWritter : LogWritter
     {
-        public FileLogWritter(IWritePolicy writePolicy) : base(writePolicy) { }
+        public FileLogWritter(ICanWrite writePolicy) : base(writePolicy) { }
 
         protected override void Write(string message)
         {
@@ -77,9 +77,9 @@
 
     internal abstract class LogWritter : ILogger
     {
-        protected readonly IWritePolicy _writePolicy;
+        protected readonly ICanWrite _writePolicy;
 
-        public LogWritter(IWritePolicy writePolicy)
+        public LogWritter(ICanWrite writePolicy)
         {
             _writePolicy = writePolicy;
         }
@@ -92,5 +92,23 @@
         }
 
         protected abstract void Write(string message);
+    }
+
+    internal class LogsWriter : ILogger
+    {
+        private readonly ILogger[] _loggers;
+
+        public LogsWriter(params ILogger[] loggers)
+        {
+            _loggers = loggers;
+        }
+
+        public void WriteMessage(string message)
+        {
+            foreach (var logger in _loggers)
+            {
+                logger.WriteMessage(message);
+            }
+        }
     }
 }
